17689f2a408e41b46eb14a56391c6fd96ad1f4f4
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-07-06 14:25:31 -0300

    .

--- app/db.js
+++
@@ -1,6 +0,0 @@
-import postgres from 'postgres'
-
-const connectionString = 'postgresql://postgres:batataassada2@db.zbqtyvduhnfivtcjrnrp.supabase.co:5432/postgres'
-const sql = postgres(connectionString)
-
-export default sql
